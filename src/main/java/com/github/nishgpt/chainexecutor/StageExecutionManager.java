303b3c98c71ee1e250df07f237f9db49a44e28bc
piyush.suman <piyush.suman@phonepe.com>
2023-05-02 14:58:28 +0530

    get next not completed stage fix

--- src/main/java/com/github/nishgpt/chainexecutor/StageExecutionManager.java
+++ src/main/java/com/github/nishgpt/chainexecutor/StageExecutionManager.java
@@ -161,7 +161,7 @@ public abstract class StageExecutionManager<T extends Stage, U extends Execution
     var currentStage = chainRegistry.getChainHead(chainIdentifier);
     do {
       final var executor = getExecutor(currentStage, auxiliaryKey);
-      if (!executor.getStageStatus(context).isCompleted()) {
+      if (!executor.getStageStatus(context).isCompletedOrSkipped()) {
         break;
       }
       currentStage = chainRegistry.getNextStageChain(chainIdentifier, currentStage);
